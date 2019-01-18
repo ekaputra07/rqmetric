d941ebae1d3c4a71dcc764906ddc03a5705099a6
Eka Putra <ekaputra07@gmail.com>
2019-01-19 02:58:57 +1100

    Added tabular viewer.

--- main.go
+++ main.go
@@ -6,9 +6,10 @@ import (
 )
 
 func printUsage() {
-  fmt.Printf("\n== [%v %v] ==\n", AppName, AppVersion)
+  fmt.Printf("\n== [%v %v - %v] ==\n", AppName, AppVersion, AppRepo)
   fmt.Println("\nUsage examples:")
   fmt.Println("Import log file  =>\trqmetric -import=production.log -profile=rails")
+  fmt.Println("View the report  =>\trqmetric -view=123456")
   fmt.Println("Serve the report =>\trqmetric -serve=123456")
   fmt.Println("Params help      =>\trqmetric -h\n")
 }
@@ -17,6 +18,7 @@ func main() {
   filePath := flag.String("import", "", "Path to the file that will be imported")
   profile := flag.String("profile", "rails", "Log profile to be use to parse the log line")
   serveId := flag.String("serve", "", "Import ID to be served")
+  viewId := flag.String("view", "", "Import ID to be viewed")
 
   flag.Parse()
 
@@ -24,6 +26,8 @@ func main() {
     Import(*filePath, *profile)
   } else if *serveId != "" {
     Serve(*serveId)
+  } else if *viewId != "" {
+    StartViewer(*viewId)
   } else {
     printUsage()
   }
